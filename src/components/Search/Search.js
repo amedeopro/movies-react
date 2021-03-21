ffd8ef1bc03070cd74849147be93fd2908b597ae
Amedeo Pro <amedeopro@me.com>
2021-03-21 12:56:52 +0100

    insert popular film

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,16 +1,32 @@
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 import './Search.scss'
 
 function Search() {
 
     const [film, setFilm] = useState('');
     const [filmList, setFilmList] = useState([]);
+    
+    const [popularFilm, setPopularFilm] = useState([]);
 
   const callFilmApi = async (movie) => {
     const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=fd686ababf68788e8d87639cef87259e&language=it-IT&query=${movie}`)
     const json = await response.json();
     setFilmList([json]);
 };
+
+const callFilmPopular = async () => {
+    const response = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=fd686ababf68788e8d87639cef87259e&language=it-IT');
+    const json = await response.json();
+    setPopularFilm([json]);
+    console.log(popularFilm)
+}
+
+useEffect(() => {
+    callFilmPopular()
+    console.log(filmList)
+},[])
+
+
 
 return (
     <div className="Search">
@@ -25,8 +41,31 @@ return (
         </div>
         <div className="row">
             <div className="col-lg-12">
-                <hr/>
-                <div className="d-flex justify-content-around flex-row flex-wrap">
+            <hr/>
+            
+            <div className="d-flex justify-content-around flex-row flex-wrap">
+                {filmList.length == 0 ? popularFilm.map(
+                    x => x.results.map((y,index) =>
+                            <div key={y.id} className="card mb-3" style={{width: "18rem"}}>
+                                <img src={`https://image.tmdb.org/t/p/w342/${y.poster_path}`} alt={y.title} className="card-img-top"/>
+                                <div className="card-body">
+                                    <h5 className="card-title">{y.title}</h5>
+                                    <p className="card-text">{y.overview.substring(0, 150) + '...'}</p>
+                                </div>
+                            </div>
+                )) : filmList.map(
+                    x => x.results.map((y,index) =>
+                            <div key={y.id} className="card mb-3" style={{width: "18rem"}}>
+                                <img src={`https://image.tmdb.org/t/p/w342/${y.poster_path}`} alt={y.title} className="card-img-top"/>
+                                <div className="card-body">
+                                    <h5 className="card-title">{y.title}</h5>
+                                    <p className="card-text">{y.overview.substring(0, 150) + '...'}</p>
+                                </div>
+                            </div>
+                ))}
+                </div>           
+                 
+                {/* <div className="d-flex justify-content-around flex-row flex-wrap">
                 {filmList.map(
                     x => x.results.map((y,index) =>
                             <div key={y.id} className="card mb-3" style={{width: "18rem"}}>
@@ -37,7 +76,7 @@ return (
                                 </div>
                             </div>
                 ))}
-                </div>
+                </div> */}
             </div>
         </div>
     </div>
